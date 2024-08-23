08b1893fbc2ee3fd3dee5e6ad381698521e94292
sam <samcormode@gmail.com>
2024-08-23 12:46:23 -0700

    init & comments for data line of board rep

--- chesskers.py
+++ chesskers.py
@@ -2,6 +2,18 @@
 
 class Board:
     def __init__(self):
+        self.squares = [[]]
+        self.from_string("""\
+put other data here
+r n b q k b n r
+p p p p p p p p
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+P P P P P P P P
+R N B Q K B N R
+        """)
         pass
     def from_string(self, string):
         counter = 0
@@ -18,8 +30,18 @@ class Board:
             "r": -4,
             "b": -3,
             "n": -2,
-            "p": -1,
+            "p": -1
         }
+        data_line = string.split('\n', 1)[0]
+        # data_line format wc,bc,ep,tn
+        # C for can, N no castle.
+        # ep is of form RCR'C' where R is a number indicating row, C is number indicating column
+        #   R' and C' are the same.
+        #   RC indicated square is one that can be taken over
+        #   R'C' square is the one the pawn that will be removed is
+        # tn is an int representing turn number (even numbers mean its blacks turn, odd means its whites)
+        #   starts at 1
+        string = string.split('\n', 1)[1]
         for token in string.split():
             x = counter % 8
             y = counter // 8
